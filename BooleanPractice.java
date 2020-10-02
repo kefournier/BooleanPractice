e9c97de6942a7bb7156b438d6e8e48fcabd63c52
kefournier <kfournier@hopkinton.k12.ma.us>
2020-10-01 21:16:14 -0400

    first commit

---
+++ BooleanPractice.java
@@ -0,0 +1,19 @@
+
+/**
+ * Some Boolean practice
+ *
+ * @author Mrs. Fournier
+ * @version September 24, 2020
+ */
+public class BooleanPractice
+{
+    public static void main(String[] args){
+        
+    boolean isTrue = false;
+    double money = 9999.99;
+    
+    System.out.println("The rumor of the CS department at HHS having a classroom budget of " + money + " is " + isTrue + ".");
+    System.out.println("The CS classrooms wish they had a budget of " + money + " so they ");
+    System.out.println("could buy all sorts of gadgets and computer \"stuff\" to help them learn.");
+}
+}
